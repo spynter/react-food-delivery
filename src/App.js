4b5b27cfdba0423f417da39fbbf1579945a6c456
jose <spynter123@gmail.com>
2023-03-04 20:05:45 -0400

    cambios realizados al heading y nuevos elementos integrados

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import { Header, MainContainer, CreateContainer } from './componentes';
 const App = () => {
   return (
 
-    <AnimatePresence>
+    <AnimatePresence mode='wait'>
       <div className='w-screen h-auto flex flex-col bg-primary'>
         <Header />
 
-        <main className="mt-24 p-8 w-full">
+        <main className="mt-16 md:mt-20 px-16 py-4 w-full">
           <Routes>
             <Route path="/" element={<MainContainer />} />
             <Route path="/createItem" element={<CreateContainer />} />
